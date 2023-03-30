7df0febf9f535122b0a4fecc44b2a29b4c7c90ab
Ramón Sepulveda <raymsa41@gmail.com>
2023-03-30 01:31:30 -0500

    clean

--- src/app.js
+++ src/app.js
@@ -31,20 +31,6 @@ app.use((err, req, res, next) => {
     return
 })
 
-/**
- * FIX ME!
- * @returns contract by id
- */
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
-    const {Contract} = req.app.get('models')
-    const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
-    if(!contract) return res.status(404).end()
-    res.json(contract)
-})
-
-// use increment or decrement to update the balance
-
 
 const foo = async () => {
     const profile = await Profile.findOne({
