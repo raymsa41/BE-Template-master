99ad0ffbffec5690408872248120bcdf2e1dbfc1
Ramón Sepulveda <raymsa41@gmail.com>
2023-03-30 10:44:03 -0500

    format logs

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -2,6 +2,10 @@ const winston = require('winston')
 
 const logger = winston.createLogger({
 	transports: [new winston.transports.Console()],
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.prettyPrint()
+    )
 })
 
 module.exports = logger
